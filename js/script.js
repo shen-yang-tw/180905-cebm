c92f87b6af1ab83bb07dc0fd0f0c8824fe95dfc0
Shen Yang <shen.yang.work.tw@gmail.com>
2018-12-27 16:28:01 +0800

    1227

--- js/script.js
+++ js/script.js
@@ -1,25 +1,62 @@
-//Checkbox toggle check all
-var click = 0;
-
-function toggleCheck(cs) {
-  var i, el = document.querySelectorAll(cs);
-  click = click + 1; //click times
-  //--set all input checked & unchecked--
-  if (click % 2 == 0) {
-    //if 'select all' unchecked
-    for (i = 0; i < el.length; i++) {
-      el[i].checked = false;
+//Input Check with Check All - <input type="checkbox" onchange="inputCheck(this,'.listCheck','.checkAll')">
+function inputCheck(thisCheck, thisClass, selectorCheckAll) {
+  var i, n = 0,
+    inputClass = document.querySelectorAll(thisClass),
+    checkAll = document.querySelector(selectorCheckAll);
+  for (i = 0; i < inputClass.length; i++) {
+    if (inputClass[i].checked == true) {
+      n = n + 1; //count input checked number
     }
+  }
+  if (thisCheck.checked == false) {
+    checkAll.checked = false;
+    checkAll.classList.remove("checked");
+    thisCheck.offsetParent.classList.remove("checked"); //the nearest ancestor that has a position other than static
+    //parent el</li> remove class "checked" when input unchecked
+  } else if (thisCheck.checked == true && n == inputClass.length) {
+    checkAll.checked = true;
+    checkAll.classList.add("checked");
   } else {
+    thisCheck.offsetParent.classList.add("checked");
+    //parent el</li> add class "checked" when input checked
+  }
+}
+
+//Checkbox toggle check all
+function toggleCheckAll(thisClick, inputClass) {
+  //thisClick means the "owner" and CANNOT use "this" that means the Global object "Window"
+  thisClick.classList.toggle("checked");
+  var i, el = document.querySelectorAll(inputClass);
+  //--set all input checked & unchecked--
+  if (thisClick.classList.contains("checked")) {
     //if 'select all' checked
     for (i = 0; i < el.length; i++) {
       el[i].checked = true;
+      el[i].offsetParent.classList.add("checked");
+      //parent el</li> add class "checked" when input checked
+    }
+  } else {
+    //if 'select all' unchecked
+    for (i = 0; i < el.length; i++) {
+      el[i].checked = false;
+      el[i].offsetParent.classList.remove("checked");
+      //parent el</li> remove class "checked" when input unchecked
     }
   }
 }
 
+//Remove all checked
+function removeAll(parent) {
+  var Parent = document.querySelector(parent);
+  var Children = Parent.querySelectorAll('.checked'); //not working on "var Children = Parent.children;"
+  for (i = 0; i < Children.length; i++) {
+    Parent.removeChild(Children[i]);
+  }
+}
+
 //Toggle Input Type (Password Visibility) with onclick="togglePassword('>input')"
-function togglePassword(cs) { //cs means CSS selectors
+function togglePassword(cs) {
+  //cs means CSS selectors
   var i, el = document.querySelectorAll(cs);
   for (i = 0; i < el.length; i++) {
     if (el[i].type === "password") {
@@ -30,7 +67,7 @@ function togglePassword(cs) { //cs means CSS selectors
   }
 }
 
-//Input Reset
+//Input Value Reset empty
 function inputReset(cs) {
   var i, el = document.querySelectorAll(cs);
   for (i = 0; i < el.length; i++) {
@@ -38,7 +75,14 @@ function inputReset(cs) {
   }
 }
 
+//Clone
+function addCloneBlock(cloneBlock) {
+  var cln = document.querySelector(cloneBlock).firstElementChild.cloneNode(true);
+  cln.classList.remove("uk-hidden");
+  document.querySelector(cloneBlock).appendChild(cln);
+}
 
+//--------------- end js ----------------------------------------------------------//
 
 //Gototop fadeIn & fadeOut on scrollTop / scroll
 $("[uk-totop]").hide();
@@ -55,27 +99,19 @@ $(window).on("load", function() {
   $(".year").text(new Date().getFullYear());
 
   //Remove parent if child empty
-  $("p:empty, h1:empty, h2:empty, h3:empty, h4:empty, h5:empty, h6:empty")
-    .parent(":empty")
-    .remove();
+  $("p:empty, h1:empty, h2:empty, h3:empty, h4:empty, h5:empty, h6:empty").parent(":empty").remove();
   //Remove if empty
-  $(
-    "p:empty, h1:empty, h2:empty, h3:empty, h4:empty, h5:empty, h6:empty, .ifEmpty:empty"
-  ).remove();
+  $("p:empty, h1:empty, h2:empty, h3:empty, h4:empty, h5:empty, h6:empty, .ifEmpty:empty, .ifonlychild:only-child:last").remove();
 
   //font resize
   if ($(".btnFontSizeS").is(".active")) {
     $("html").removeClass("fontSizeM fontSizeL");
   }
   if ($(".btnFontSizeM").is(".active")) {
-    $("html")
-      .removeClass("fontSizeL")
-      .addClass("fontSizeM");
+    $("html").removeClass("fontSizeL").addClass("fontSizeM");
   }
   if ($(".btnFontSizeL").is(".active")) {
-    $("html")
-      .removeClass("fontSizeM")
-      .addClass("fontSizeL");
+    $("html").removeClass("fontSizeM").addClass("fontSizeL");
   }
   $(".btnFontSize").click(function() {
     $(".btnFontSize").removeClass("active");
@@ -83,57 +119,35 @@ $(window).on("load", function() {
   });
   $(".btnFontSizeL>:only-child").click(function() {
     // $('.font_resize').removeClass('font_.btnFontSizeM').addClass('font_large');
-    $("html")
-      .removeClass("fontSizeM")
-      .addClass("fontSizeL");
+    $("html").removeClass("fontSizeM").addClass("fontSizeL");
   });
   $(".btnFontSizeM>:only-child").click(function() {
     // $('.font_resize').removeClass('font_large').addClass('font_.btnFontSizeM');
-    $("html")
-      .removeClass("fontSizeL")
-      .addClass("fontSizeM");
+    $("html").removeClass("fontSizeL").addClass("fontSizeM");
   });
   $(".btnFontSizeS>:only-child").click(function() {
     // $('.font_resize').removeClass('font_.btnFontSizeM font_large');
     $("html").removeClass("fontSizeM fontSizeL");
   });
 
-  //Search input & two levels of select - change parent select option to display & enable child select option
+  //(02-empirical2) Search input & two levels of select - change parent select option to display & enable child select option
   $("#select_dep").change(function() {
     //parent select
-    $(".select_div")
-      .prop("disabled", "disabled")
-      .prop("hidden", "hidden"); //all child select hidden
-    if (
-      $(this)
-      .children(":first-child")
-      .is(":selected")
-    ) {
+    $(".select_div").prop("disabled", "disabled").prop("hidden", "hidden"); //all child select hidden
+    if ($(this).children(":first-child").is(":selected")) {
       //if 1st option selected
-      $(".select_div:first")
-        .prop("disabled", "disabled")
-        .prop("hidden", false); //default first child select show & disabled
+      $(".select_div:first").prop("disabled", "disabled").prop("hidden", false); //default first child select show & disabled
       $("#search_input").val(""); //empty input text
     } else {
-      $('.select_div[data-div="' + $(this).val() + '"]')
-        .prop("disabled", false)
-        .prop("hidden", false); //child select show & enabled
+      $('.select_div[data-div="' + $(this).val() + '"]').prop("disabled", false).prop("hidden", false); //child select show & enabled
       $("#search_input").val($(this).val()); //option text selected placeed into input
     }
   });
   $(".select_div").change(function() {
     //child select
-    if (
-      $(this)
-      .children(":first-child")
-      .is(":selected")
-    ) {
+    if ($(this).children(":first-child").is(":selected")) {
       //if 1st option selected
-      if (
-        $("#select_dep")
-        .children(":first-child")
-        .is(":selected")
-      ) {
+      if ($("#select_dep").children(":first-child").is(":selected")) {
         //and if 1st option of parent select selected
         $("#search_input").val(""); //empty input text
       } else {
@@ -144,63 +158,45 @@ $(window).on("load", function() {
     }
   });
 
-  //dynamically checked & check all checkbox
-  // $(".checkAll").click(function() {
-  //   $(".listCheck").prop("checked", this.checked); //toggle check
-  // });
-
-  //for Add File - button #add cannot place in <form> or not working
-  $(".file").before($(".file").clone().addClass('uk-hidden'));
-  $("#addFile").click(function() {
-    var counter = $(".fileBlock>div").length;
-    var newFile = $(".file:first").clone().removeClass('uk-hidden');
-    if (counter < 5) {
-      $(".file:last").after(newFile);
+  //(02-empirical3, 03-knowledge2) for Add clone - button #add cannot place in <form> or not working
+  $(".clone:first").before($(".clone:first").clone().addClass("uk-hidden"));
+  $(".addClone").click(function() {
+    var clone = $(this).parent().parent(".cloneGroup").children(".cloneBlock").children(".clone");
+    var counter = clone.not(".uk-hidden").length;
+    if (counter < 4) {
+      clone.last().after($(".clone.uk-hidden").clone().removeClass("uk-hidden"));
     }
+  });
+
+  //datepicker
+  $('#form_i_date').datepicker({
+    language: "zh-TW",
+    autoclose: true,
+    todayHighlight: true
   });
 
 });
 
 $(window).on("resize load", function() {
   //Match height
-  var h_middle =
-    $(".heightJS").height() -
-    $(".heightJS>div:nth-child(2)").height() -
-    $(".heightJS>div:nth-child(4)").height();
-  $(".heightJS").css(
-    "padding-bottom",
-    $(".heightJS>div:nth-child(4)").height()
-  );
+  var h_middle = $(".heightJS").height() - $(".heightJS>div:nth-child(2)").height() - $(".heightJS>div:nth-child(4)").height();
+  $(".heightJS").css("padding-bottom", $(".heightJS>div:nth-child(4)").height());
   $(".heightJS>div:nth-child(3)").css("height", h_middle);
 
   //keep aspect ratio of image's height to width
   $(".ratio3_4 li img, .thisRatio3_4").each(function() {
-    $(this).css({
-      height: ($(this).width() * 4) / 3
-      //3/4 portrait
-    });
+    $(this).css({height: ($(this).width() * 4) / 3 }); //3/4 portrait
   });
   $(".ratio9_16 li img, .thisRatio9_16").each(function() {
-    $(this).css({
-      height: ($(this).width() * 16) / 9
-      //9/16 portrait
-    });
+    $(this).css({height: ($(this).width() * 16) / 9}); //9/16 portrait
   });
   $(".ratio4_3 li img, .thisRatio4_3").each(function() {
-    $(this).css({
-      height: ($(this).width() * 3) / 4
-      //4/3 landscape
-    });
+    $(this).css({height: ($(this).width() * 3) / 4}); //4/3 landscape
   });
   $(".ratio16_9 li img, .thisRatio16_9").each(function() {
-    $(this).css({
-      height: ($(this).width() * 9) / 16
-      //16/9 landscape
-    });
+    $(this).css({height: ($(this).width() * 9) / 16}); //16/9 landscape
   });
   $(".ratio1_1 li img, .thisRatio1_1").each(function() {
-    $(this).css({
-      height: $(this).width()
-    });
+    $(this).css({height: $(this).width()});
   });
 });
